f437f1bf3ddab2f153b6ac67d8791f12d31726a1
Friederich <siyfred1n@gmail.com>
2025-10-05 20:01:07 -0300

    add: paginas de recuperar senha

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('register/', views.register, name='register'),
+    path('recoverpassword/', views.RecoverPassword.as_view(), name='recover_password'),
+    path('recoverpassword/done/', views.RecoverPasswordDone.as_view(), name='recover_password_done'),
+    path('recoverpassword/<uidb64>/<token>/', views.RecoverPasswordConfirm.as_view(), name='recover_password_confirm'),
+    path('recoverpassword/complete/', views.RecoverPasswordComplete.as_view(), name='recover_password_complete'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 # Create your views here.
 def home(request):
@@ -8,11 +10,25 @@ def home(request):
 
 def login(request):
     form = AuthenticationForm()
-    return render(request, 'login.html', {'form': form})
+    return render(request, 'registration/login.html', {'form': form})
 
 def logout(request):
     return render(request, 'home.html')
 
 def register(request):
     form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
+    return render(request, 'registration/register.html', {'form': form})
+
+class RecoverPassword(auth_views.PasswordResetView):
+    template_name = 'registration/recover_password.html'
+    success_url = reverse_lazy('recover_password_done')
+
+class RecoverPasswordDone(auth_views.PasswordResetDoneView):
+    template_name = 'registration/recover_password_done.html'
+
+class RecoverPasswordConfirm(auth_views.PasswordResetConfirmView):
+    template_name = 'registration/recover_password_confirm.html'
+    success_url = reverse_lazy('recover_password_complete')
+
+class RecoverPasswordComplete(auth_views.PasswordResetCompleteView):
+    template_name = 'registration/recover_password_complete.html'
